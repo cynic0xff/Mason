590d544c94d98440aa5428850b493c7a8f24d70d
Scott Wren <cynics0xff@gmail.com>
2019-06-04 07:01:52 -0400

    Add profile framework html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HttpClientModule } from '@angular/common/http';
+import { 
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule
+ } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -12,7 +24,17 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
