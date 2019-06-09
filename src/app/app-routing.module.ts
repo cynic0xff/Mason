17b20cd4e0b5140ee03d49757e3ed1082b0cc885
Scott Wren <cynics0xff@gmail.com>
2019-06-08 22:10:59 -0400

    Add profile, lodge-profile, dashboard and default routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { LodgeProfileComponent } from './lodge-profile/lodge-profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'lodge-profile', component: LodgeProfileComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '',   redirectTo: '/dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
